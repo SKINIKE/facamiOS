930acd7b3a5a3047cdbe7cb8b5224e046f5c9b12
ski <sunnike1010@gmail.com>
2022-06-26 13:29:01 +0900

    명언생성기 완성

---
+++ QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by Kwangil Shin on 2022/06/26.
+//
+
+import Foundation
+
+struct Quote{
+    let contents: String
+    let name: String
+}
